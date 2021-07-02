cdf6c67383ef4a18cfaea2192b6ce835180e8b08
Daniel O <danielogunlana23@gmail.com>
2021-07-02 15:31:02 -0400

    added comment model

--- model/Post.js
+++ model/Post.js
@@ -33,6 +33,13 @@ Post.init(
         key: 'id',
       },
     },
+    comment_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'comment',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
