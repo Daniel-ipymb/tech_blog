5f1dc49e2cb60807b3d12010f7c08d848c7b3ee6
Daniel O <danielogunlana23@gmail.com>
2021-07-08 17:30:59 -0400

    added post route to comment

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, Post } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-// router.post('/', async (req,res) => {
-//   const commentData = await
-// })
+router.post('/:id', async (req,res) => {
+  try {
+    const commentData = await Comment.create({
+      comment_text: req.body.comment_text,
+      where: {
+        id : req.params.id
+      },
+    })
+    res.status(200).json(commentData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
 module.exports = router;
