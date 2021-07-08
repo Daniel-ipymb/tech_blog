091a40d7198faf9cb73b96b14366119fc5f3cd82
Daniel O <danielogunlana23@gmail.com>
2021-07-07 22:40:34 -0400

    added update and delete route for dashboard

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,5 +16,40 @@ router.post('/', async (req,res) => {
   }
 });
 
+router.post('/update/:id', async (req,res) => {
+  try {
+    const updateData = await Post.update(
+      {
+      title: req.body.title,
+      post_content: req.body.post_content
+      },
+      {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updateData)
+  } catch (error) {
+    res.json(500).json(error)
+  };
+});
+
+router.delete('/delete/:id', async (req,res) => {
+  try {
+    const deleteData = await Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    if (!deleteData) {
+      res.status(404).json({ message: 'No blog found with this id!'});
+      return;
+    }
+    res.status(200).json(deleteData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
 module.exports = router;
 
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,6 +5,7 @@ router.post('/', async (req, res) => {
   console.log(req.body)
   try {
     const userData = await User.create(req.body);
+    console.log(userData)
 
     req.session.save(() => {
       req.session.user_id = userData.id;
@@ -20,7 +21,7 @@ router.post('/', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
-
+    console.log(userData)
     if (!userData) {
       res
         .status(400)
